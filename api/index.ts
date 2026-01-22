d4fbc9b23d9212bf6fda827012fc41b4156e8ffe
Fredcx <fredmourino@gmail.com>
2026-01-22 16:48:19 -0300

    fix: categorical vercel body parser limit

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,11 @@
 import app from '../backend/src/server';
 
+export const config = {
+    api: {
+        bodyParser: {
+            sizeLimit: '10mb',
+        },
+    },
+};
+
 export default app;
